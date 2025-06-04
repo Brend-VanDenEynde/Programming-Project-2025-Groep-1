4141f19fa3ee63516d30f06d00936756b02809b7
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-06-04 11:45:41 +0200

    // nog toegevoegd zodat het gemakkelijk te begrijpen

--- career-launch/src/main.js
+++ career-launch/src/main.js
@@ -1,8 +1,12 @@
 // main.js
 import Router from './router.js';
 import './style.css';
+
+// importeer de pagina's
 import { renderHome } from './pages/home.js';
 
+
+// functie om de 404 pagina te renderen (Nog aanpassen)
 function renderNotFound(rootElement) {
     rootElement.innerHTML = `
         <h1>404 - Pagina niet gevonden</h1>
@@ -10,6 +14,7 @@ function renderNotFound(rootElement) {
     `;
 }
 
+// Definieer de routes
 const routes = {
     '/': renderHome,
     '/404': renderNotFound,
