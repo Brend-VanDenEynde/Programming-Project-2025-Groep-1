aa6401dfe34113f172b385713d8593bcb41eb34f
Shems45 <shemsedine.boughaleb@student.ehb.be>
2025-06-04 12:04:19 +0200

    home.js aangemaakt

--- career-launch/src/pages/home.js
+++ career-launch/src/pages/home.js
@@ -1,5 +1,34 @@
+//import { renderLogin } from './login.js';
+//import { renderRegister } from './register.js';
+
 export function renderHome(rootElement) {
-    rootElement.innerHTML = `
-        <h1>Home page</h1>
-    `;
+  rootElement.innerHTML = `
+    <div class="home-container">
+      <img 
+        src="/src/Images/EhB-logo-transparant.png" 
+        alt="EHB Logo" 
+        style="max-width: 200px; margin-bottom: 24px;"
+      />
+
+      <h1>Erasmus Hogeschool</h1>
+      <h2>Career Launch</h2>
+
+      <p>Geïnteresseerd?</p>
+      <button id="btn-register" class="btn">Registreer nu!</button>
+
+      <p>Al ingeschreven?</p>
+      <button id="btn-login" class="btn">Login</button>
+    </div>
+  `;
+
+  const btnReg = document.getElementById('btn-register');
+  const btnLog = document.getElementById('btn-login');
+
+  btnReg.addEventListener('click', () => {
+    renderRegister(rootElement);
+  });
+
+  btnLog.addEventListener('click', () => {
+    renderLogin(rootElement);
+  });
 }
