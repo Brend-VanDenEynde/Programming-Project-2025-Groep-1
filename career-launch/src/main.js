2ea26fcd88901316c0e9a30304a8b157ca9eb0d3
Shems45 <shemsedine.boughaleb@student.ehb.be>
2025-06-04 15:15:38 +0200

    Update main.js

--- career-launch/src/main.js
+++ career-launch/src/main.js
@@ -5,15 +5,11 @@ import './style.css';
 // importeer de pagina's
 import { renderHome } from './pages/home.js';
 import { renderRegister } from './pages/register.js';
-<<<<<<< HEAD
 import { renderLogin } from './pages/login.js';
 import { renderStudentProfiel } from './pages/student-profiel.js';
 import { renderSearchCriteriaStudent } from './pages/search-criteria-student.js';
-
-=======
 import { renderAdmin } from './pages/admin.js';
 import { renderAdminDashboard } from './pages/admin-dashboard.js';
->>>>>>> a35d99f2bde7b6bfd6905665b9492852a2e16ec9
 
 // functie om de 404 pagina te renderen (Nog aanpassen)
 function renderNotFound(rootElement) {
@@ -28,16 +24,11 @@ const routes = {
   '/': renderHome,
   '/404': renderNotFound,
   '/registreer': renderRegister,
-<<<<<<< HEAD
   '/login' : renderLogin,
   '/Student/Student-Profiel': renderStudentProfiel,
-  '/Student/Zoek-Criteria': renderSearchCriteriaStudent
-
-
-=======
+  '/Student/Zoek-Criteria': renderSearchCriteriaStudent,
   '/admin': renderAdmin,
-  '/admin-dashboard': renderAdminDashboard,
->>>>>>> a35d99f2bde7b6bfd6905665b9492852a2e16ec9
+  '/admin/admin-dashboard': renderAdminDashboard,
 };
 
 const router = new Router(routes);
