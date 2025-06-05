1428fa6bbad1f37317173698ba469ec94e52fbbf
Pringels4u <mathiasdb20@gmail.com>
2025-06-05 15:50:42 +0200

    Student opleiding pagina bijgemaakt

--- career-launch/src/pages/student-opleiding.js
+++ career-launch/src/pages/student-opleiding.js
@@ -2,31 +2,32 @@ import './student-register.css';
 
 export function renderStudentOpleiding(rootElement) {
   rootElement.innerHTML = `
-    <div class="page-wrapper">
-      <main class="form-box">
-        <button class="close-button">X</button>
+    <div style="min-height: 100vh; display: flex; flex-direction: column;">
+    <main class="form-container">
+      <button class="back-button">← Terug</button>
 
-        <h3>Ik ben een</h3>
+      <h3>Ik ben een</h3>
         <div class="radio-group">
           <label><input type="radio" name="jaar" value="1"> 1ste jaar</label>
           <label><input type="radio" name="jaar" value="2"> 2de jaar</label>
           <label><input type="radio" name="jaar" value="3"> 3de jaar</label>
         </div>
 
-        <select class="opleiding-select">
+
+      <select class="opleiding-select">
           <option selected disabled>Opleiding</option>
           <option>Business IT</option>
           <option>Networks & Security</option>
           <option>Software Engineering</option>
           <option>Intelligent Robotics</option>
-        </select>
+      </select>
 
-        <button class="save-button">Save</button>
-      </main>
+      <button class="next-button">SAVE</button>
+    </main>
 
-      <footer class="footer">
-        <a href="#">Privacy Policy</a> | <a href="#">Contacteer Ons</a>
-      </footer>
-    </div>
+    <footer class="footer">
+      <a href="#">Privacy Policy</a> | <a href="#">Contacteer Ons</a>
+    </footer>
+  </div>
   `;
 }
