f70428b25809028c1e405531421b534998ba0bfc
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-06-12 09:47:44 +0200

    Update main.js

--- career-launch/src/main.js
+++ career-launch/src/main.js
@@ -15,12 +15,12 @@ import { renderQRPopup } from './pages/Student/student-qr-popup.js';
 import { renderSpeeddates } from './pages/Student/student-speeddates.js';
 import { renderSpeeddatesRequests } from './pages/Student/student-speeddates-verzoeken.js';
 import { showSettingsPopup } from './pages/Student/student-settings.js';
-import { renderAdminIngeschrevenStudenten } from './pages/Admin/admin-ingeschreven-studenten.js';
-import { renderAdminIngeschrevenBedrijven } from './pages/Admin/admin-ingeschreven-bedrijven.js';
-import { renderAdminBedrijvenInBehandeling } from './pages/Admin/admin-bedrijven-in-behandeling.js';
+import { renderAdminIngeschrevenStudenten } from './pages/admin/admin-ingeschreven-studenten.js';
+import { renderAdminIngeschrevenBedrijven } from './pages/admin/admin-ingeschreven-bedrijven.js';
+import { renderAdminBedrijvenInBehandeling } from './pages/admin/admin-bedrijven-in-behandeling.js';
 import { renderAdminStudentDetail } from './pages/Admin/admin-student-detail.js';
 import { renderAdminCompanyDetail } from './pages/Admin/admin-company-detail.js';
-import { renderAdminProcessingCompanyDetail } from './pages/Admin/admin-dashboard.js';
+import { renderAdminProcessingCompanyDetail } from './pages/admin/admin-dashboard.js';
 
 function renderNotFound(rootElement) {
   rootElement.innerHTML = `
