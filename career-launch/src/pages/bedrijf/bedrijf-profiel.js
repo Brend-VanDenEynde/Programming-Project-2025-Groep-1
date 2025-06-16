4b89163ecb2e182cba2c76b3c17a7784f576d6da
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-06-16 13:45:30 +0200

    Update bedrijf-profiel.js

--- career-launch/src/pages/bedrijf/bedrijf-profiel.js
+++ career-launch/src/pages/bedrijf/bedrijf-profiel.js
@@ -373,8 +373,6 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 // Dummy file for bedrijf-profiel.js
-export function renderBedrijfProfiel() {
-  return 'Bedrijf Profiel Dummy';
-}
+
 export default renderBedrijfProfiel;
 
